2df71095133b2184584dda9e98f425a8fda1e8b0
AliHaidry <alihaidry11@gmail.com>
2021-04-07 16:50:31 -0400

    code update with 'creating route handlers'

--- authy/src/index.ts
+++ authy/src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
 import { json } from "body-parser";
+import {currentUserRouter} from './routes/current-user';
 
 const app = express();
 app.use(json());
 
 
 /** Routes Setup */
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there !');
-});
+app.use(currentUserRouter);
 
 
 /** Port Setup */
