1e95f4ab6f46f65d7e28b8931c7a38bca3c84bfb
AliHaidry <alihaidry11@gmail.com>
2021-04-07 16:59:35 -0400

    code update with 'scaffolding routes'

--- authy/src/index.ts
+++ authy/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { json } from "body-parser";
 import {currentUserRouter} from './routes/current-user';
+import {signinRouter} from './routes/signin';
+import {signupRouter} from './routes/signup';
+import {signoutRouter} from './routes/signout';
 
 const app = express();
 app.use(json());
@@ -8,6 +11,11 @@ app.use(json());
 
 /** Routes Setup */
 app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+
 
 
 /** Port Setup */
--- authy/src/routes/current-user.ts
+++ authy/src/routes/current-user.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", () => {});
+router.get("/api/users/currentuser", (req,res) => {
+    res.send('Hi There Amigos!');
+});
 
 export { router as currentUserRouter };
