3029ad3b908f0c3a1b1c0d14fbc04029b6ffeb4a
AliHaidry <alihaidry11@gmail.com>
2021-04-08 15:48:43 -0400

    code update with 'Handling Errors'

--- authy/src/routes/signup.ts
+++ authy/src/routes/signup.ts
@@ -1,25 +1,29 @@
-import express, {Request, Response} from "express";
-import { body } from "express-validator";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", [
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid'),
-    body('password')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Password must be between 4 and 20 characters'),    
-],
-(req : Request, res : Response) => {
-  const { email, password } = req.body;
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-  if (!email || typeof email !== "string") {
-    res.send(400).send("Provide a valid email");
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log("Creating a user....");
+    res.send({});
   }
-
-  // new User({email, password})
-});
+);
 
 export { router as signupRouter };
