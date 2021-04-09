13f8d5c629c4ebd6672d3cde1283dfa9abee167b
AliHaidry <alihaidry11@gmail.com>
2021-04-09 09:29:34 -0400

    code update with 'Creating errors to responses'

--- authy/src/middlewares/error-handler.ts
+++ authy/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +8,19 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-    console.log('Something went wrong', err);
+    if(err instanceof RequestValidationError){
+      const formattedErrors = err.errors.map(error => {
+        return {message: error.msg, field: error.param};
+      });
+
+      return res.status(400).send({errors: formattedErrors});
+    }
+
+    if(err instanceof DatabaseConnectionError){
+      return res.status(500).send({errors:[{message: err.reason}] });
+    }
 
     res.status(400).send({
-        message: err.message
+        errors: [{message: 'Something went wrong'}]
     });
 };
