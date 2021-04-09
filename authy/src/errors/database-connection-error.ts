ca77724ef71e889ffd9fae64ba2bca7499938f4a
AliHaidry <alihaidry11@gmail.com>
2021-04-09 09:46:01 -0400

    code update with 'Moving logic into errors'

--- authy/src/errors/database-connection-error.ts
+++ authy/src/errors/database-connection-error.ts
@@ -1,8 +1,15 @@
 export class DatabaseConnectionError extends Error {
+    statusCode = 500;
     reason = 'Error connecting to database';
     constructor(){
         super();
 
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors(){
+        return[
+            { message: this.reason}
+        ];
     }
 }
