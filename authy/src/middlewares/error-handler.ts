9cbd02b22b6abd05c6092f6f6eb7a219515f09cc
AliHaidry <alihaidry11@gmail.com>
2021-04-09 10:14:51 -0400

    code update with 'Final error related code'

--- authy/src/middlewares/error-handler.ts
+++ authy/src/middlewares/error-handler.ts
@@ -1,18 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
-
+import {CustomError} from "../errors/custom-error";
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-    if(err instanceof RequestValidationError){
-      return res.status(err.statusCode).send({errors: err.serializeErrors()});
-    }
-
-    if(err instanceof DatabaseConnectionError){
+    if(err instanceof CustomError){
       return res.status(err.statusCode).send({errors: err.serializeErrors()});
     }
 
