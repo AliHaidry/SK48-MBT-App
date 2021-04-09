e61f14e12fdd60be0d3e7babb58765d9f669b2b7
AliHaidry <alihaidry11@gmail.com>
2021-04-09 12:28:47 -0400

    code update with 'Type checking user properties'

--- authy/src/models/users.ts
+++ authy/src/models/users.ts
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
+// An interface that describes the properties
+// that are required to create a new User
+// 'Attrs' is short for attributes 
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -13,4 +21,9 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-export { User };
+const buildUser = (attrs: UserAttrs) => {
+    return new User(attrs);
+};
+
+
+export { User, buildUser };
