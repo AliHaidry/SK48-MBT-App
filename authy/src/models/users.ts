c39d0eabf3be507c17cb907b13149aef4c4fb533
AliHaidry <alihaidry11@gmail.com>
2021-04-09 12:37:30 -0400

    code update with 'Adding Static Properties to a Model'

--- authy/src/models/users.ts
+++ authy/src/models/users.ts
@@ -8,6 +8,13 @@ interface UserAttrs {
     password: string;
 }
 
+// An interfcae that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<any> {
+    build(attrs: UserAttrs): any;
+}
+
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -19,11 +26,11 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model('User', userSchema);
 
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
 };
 
+const User = mongoose.model<any, UserModel>('User', userSchema);
 
-export { User, buildUser };
+export { User};
