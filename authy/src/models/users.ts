ecab95e63a4f11dfd1f82d26a986234675b83dc6
AliHaidry <alihaidry11@gmail.com>
2021-04-09 15:20:50 -0400

    code update with 'Mongoose Pre-Save Hooks'

--- authy/src/models/users.ts
+++ authy/src/models/users.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // An interface that describes the properties
 // that are required to create a new User
@@ -30,6 +31,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+// callback function
+userSchema.pre('save', async function (done) {
+    if (this.isModified('password')){
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+    done();
 });
 
 
