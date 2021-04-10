ca07e430d7af74f252c7a93e37e944b16b0d842d
AliHaidry <alihaidry11@gmail.com>
2021-04-10 15:48:32 -0400

    code update with 'Generating a JWT'

--- authy/src/routes/signup.ts
+++ authy/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from 'jsonwebtoken';
+
 import { User } from "../models/users";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
@@ -32,6 +34,16 @@ router.post(
 
     const user = User.build({email, password});
     await user.save(); // this will persist them to mongoDB.
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id:user.id,
+      email:user.email
+    },'asdf');
+    // Store it on session object
+    req.session = {
+      jwt: userJwt
+    };
 
     res.status(201).send(user);
   }
