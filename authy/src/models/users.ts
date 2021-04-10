cfefd2f4dd378fef4d2d56ece114d55a530253be
AliHaidry <alihaidry11@gmail.com>
2021-04-10 17:18:24 -0400

    code update with 'Formatting JSON Properties'

--- authy/src/models/users.ts
+++ authy/src/models/users.ts
@@ -31,6 +31,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+}, {
+    toJSON: {
+        transform(doc, ret){
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.password;
+            delete ret.__v;
+        }
+    }
 });
 
 // callback function
