cef458e2b9d64ba1af819c68d04fc5abaa80c95e
AliHaidry <alihaidry11@gmail.com>
2021-04-11 19:57:48 -0400

    code update 'Quick Signin Test'

--- authy/src/routes/signin.ts
+++ authy/src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express";
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { Password } from '../services/password';
+import { User } from '../models/users';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 
 const router = express.Router();
@@ -16,8 +21,30 @@ router.post("/api/users/signin",
         .withMessage('You must supply a password')
 ], 
 validateRequest,
-(req: Request,res : Response) => {
-    
+async (req: Request,res : Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if(!existingUser) {
+        throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+    if(!passwordsMatch){
+        throw new BadRequestError('Invalid credentials');
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+        id:existingUser.id,
+        email:existingUser.email
+      }, process.env.JWT_KEY!); // we know that this env - variable is defined.
+      // Store it on session object
+      req.session = {
+        jwt: userJwt
+      };
+  
+      res.status(201).send(existingUser);
 });
 
 export { router as signinRouter };
