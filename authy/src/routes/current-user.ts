878799c53c6794a0e388d157693298e9097dfe39
AliHaidry <alihaidry11@gmail.com>
2021-04-12 08:24:15 -0400

    code update with 'Returning the current user'

--- authy/src/routes/current-user.ts
+++ authy/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req,res) => {
-    res.send('Hi There Amigos!');
+router.get("/api/users/currentuser", (req, res) => {
+  // the ? is checking if some internal property exists or not.
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try{
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!); // the ! tells the typescript not to worry about it.
+    res.send({ currentUser: payload });
+  }
+  catch (err) {
+    res.send({ currentUser: null });
+  }
+ 
 });
 
 export { router as currentUserRouter };
