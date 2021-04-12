b6ab23e1ae7768b588f5b7faf729932a963a8069
AliHaidry <alihaidry11@gmail.com>
2021-04-12 11:38:30 -0400

    code upate with 'Testing Invalid Input'

--- authy/src/routes/__test__/signup.test.ts
+++ authy/src/routes/__test__/signup.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-// description for the test.
+// 'it' description for the test.
 it('return a 201 on successful signup', async () => {
     return request(app)
         .post('/api/users/signup')
@@ -10,4 +10,41 @@ it('return a 201 on successful signup', async () => {
             password: 'password'
         })
         .expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'testtest.com',
+        password: 'password'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'testtest.com',
+        password: 'p'
+    })
+    .expect(400);
+});
+
+
+it('returns a 400 with missing email and password', async () => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com'
+    })
+    .expect(400);
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        password: 'password'
+    })
+    .expect(400);
 });
