ad33bcfa8e80a6513f21801d2c464dc446a70490
AliHaidry <alihaidry11@gmail.com>
2021-04-12 08:57:59 -0400

    code update with 'Augmenting Type Definitions'

--- authy/src/routes/current-user.ts
+++ authy/src/routes/current-user.ts
@@ -1,21 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 
+import { currentUser } from "../middlewares/current-user";
+
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  // the ? is checking if some internal property exists or not.
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
+router.get("/api/users/currentuser", currentUser, (req, res) => {
 
-  try{
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!); // the ! tells the typescript not to worry about it.
-    res.send({ currentUser: payload });
-  }
-  catch (err) {
-    res.send({ currentUser: null });
-  }
+  res.send({ currentUser: req.currentUser || null });
  
 });
 
