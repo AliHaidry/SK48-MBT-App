effb0a764508a0b2a162d3ecf977c92509e1d135
AliHaidry <alihaidry11@gmail.com>
2021-04-12 12:40:21 -0400

    code upate with 'Auth Helper Function'

--- authy/src/test/setup.ts
+++ authy/src/test/setup.ts
@@ -1,6 +1,16 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from 'supertest';
 import { app } from "../app";
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin(): Promise<string[]>;
+        }
+    }
+}
+
 
 let mongo: any;
 beforeAll(async () => {
@@ -27,3 +37,20 @@ afterAll(async () =>{
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email, password
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+};
