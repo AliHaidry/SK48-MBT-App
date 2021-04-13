6753d5d92c8440a4959f361a752e6beaa84f2155
AliHaidry <alihaidry11@gmail.com>
2021-04-13 14:42:43 -0400

    Code update with 'When is GetInitialProp is called?'

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
 
 const LandingPage = ({currentUser}) => {
-  console.log(currentUser);
-  axios.get('/api/users/currentuser');
+  //console.log(currentUser);
+  //axios.get('/api/users/currentuser');
   return <h1>Landing Page</h1>;
 };
 
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async () => {
 
+  // const response = await axios.get('/api/users/currentuser');
+  // return response.data;
+  console.log('I was executed');
+  return {};
 
-//   return response.data;
-// }
+}
 
 
 export default LandingPage;
