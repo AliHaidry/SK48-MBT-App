58a12127700bdc38d94ea1ea36256623d82d7d96
AliHaidry <alihaidry11@gmail.com>
2021-04-13 15:22:21 -0400

    Code update with 'Specifying the Host'

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,15 +1,15 @@
-import axios from "axios";
-import { useState } from "react";
+import axios from 'axios';
+import { useState } from 'react';
 
 export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
     try {
-      setErrors(null);  
+      setErrors(null);
       const response = await axios[method](url, body);
 
-      if(onSuccess) {
+      if (onSuccess) {
         onSuccess(response.data);
       }
 
@@ -17,9 +17,9 @@ export default ({ url, method, body, onSuccess }) => {
     } catch (err) {
       setErrors(
         <div className="alert alert-danger">
-          <h4>Oops...</h4>
+          <h4>Ooops....</h4>
           <ul className="my-0">
-            {err.response.data.errors.map((err) => (
+            {err.response.data.errors.map(err => (
               <li key={err.message}>{err.message}</li>
             ))}
           </ul>
