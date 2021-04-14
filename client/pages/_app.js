30451d41eb1c908eba68b165f460aa391aaed5c6
AliHaidry <alihaidry11@gmail.com>
2021-04-14 11:06:01 -0400

    Code update with 'Moving GetInitialProps'

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
 // Defined our own custom app component.
-export default ({ Component, pageProps }) => {
-    return <div>
-    <h1>Header!</h1>
-    <Component {...pageProps} />
-    </div> 
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = () => {
+        
+};
+
+export default AppComponent;
