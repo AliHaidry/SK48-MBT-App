5abca67228aa6e6d86f3bd6343fbf448129327e0
AliHaidry <alihaidry11@gmail.com>
2021-04-14 11:01:55 -0400

    Code update with 'A Reusable Header '

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,5 +2,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 // Defined our own custom app component.
 export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+    return <div>
+    <h1>Header!</h1>
+    <Component {...pageProps} />
+    </div> 
 };
