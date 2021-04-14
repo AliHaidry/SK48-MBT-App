e8e29cfec4461afae3a5ea458a5c693267671353
AliHaidry <alihaidry11@gmail.com>
2021-04-14 10:49:48 -0400

    Code update with 'Content on the Landing Page'

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,16 @@
-import buildClient from '../api/build-client';
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
-  // axios.get('/api/users/currentuser');
-  console.log(currentUser);
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context)
-  const { data } = await client.get('/api/users/currentuser');
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
 
   return data;
 };
