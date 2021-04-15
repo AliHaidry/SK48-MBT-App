0e06cb2f764b70c88e92a02b5a53ef7beb1f8ead
AliHaidry <alihaidry11@gmail.com>
2021-04-15 13:40:45 -0400

    Code update with 'Creation via Route Handler'

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
     const response = await request(app)
@@ -67,12 +68,23 @@ it('returns an error if an invalid price is provided', async () => {
 
 
 it('creates a ticket with valid inputs', async () => {
-   // add in a check to make sure a ticket was saved. 
-   await request(app)
+   
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0);
+    const title = 'Books';
+
+
+    await request(app)
         .post('/api/tickets')
+        .set('Cookie', global.signin())
         .send({
             title: 'Books',
             price: 20,
         }) 
         .expect(201);
+
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+    expect(tickets[0].price).toEqual(20);
+    expect(tickets[0].title).toEqual(title);
 });
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@ahtickcon/common';
+import  { Ticket } from '../models/ticket';
 
 
 const  router = express.Router();
@@ -15,8 +16,18 @@ router.post('/api/tickets', requireAuth, [
         .withMessage('Price is must be greater than 0'),
 
     
-], validateRequest, (req: Request, res: Response) => {
-    res.sendStatus(200);
+], validateRequest, async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+        title,
+        price,
+        userId: req.currentUser!.id
+    });
+    await ticket.save();
+
+
+    res.status(201).send(ticket);
 });
 
 export { router as  createTicketRouter }
