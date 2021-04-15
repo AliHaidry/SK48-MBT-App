9e4aeda3567b484f73e744f1dd3a187a550f5d8c
AliHaidry <alihaidry11@gmail.com>
2021-04-15 13:15:10 -0400

    Code update with 'Remainder on Mongoose with Typescript'

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -67,5 +67,12 @@ it('returns an error if an invalid price is provided', async () => {
 
 
 it('creates a ticket with valid inputs', async () => {
-    
+   // add in a check to make sure a ticket was saved. 
+   await request(app)
+        .post('/api/tickets')
+        .send({
+            title: 'Books',
+            price: 20,
+        }) 
+        .expect(201);
 });
