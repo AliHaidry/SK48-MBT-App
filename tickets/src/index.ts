2f45c10e37003fb3e8d819315648d3a6f7fe9cf3
AliHaidry <alihaidry11@gmail.com>
2021-04-15 11:18:54 -0400

    Code update with 'MONGO Correction URI'

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,9 +5,14 @@ const start = async () => {
   if(!process.env.JWT_KEY){
     throw new Error("JWT_KEY must be defined");
   }
+
+  if(!process.env.MONGO_URI){
+    throw new Error("MONGO_URI must be defined");
+    
+  }
   
   try {
-    await mongoose.connect("mongodb://authy-mongo-srv:27017/authy", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
