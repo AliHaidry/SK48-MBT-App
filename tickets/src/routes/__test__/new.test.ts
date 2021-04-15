63fde74744d2c75f4bf9038cb74993ce0167d795
AliHaidry <alihaidry11@gmail.com>
2021-04-15 13:03:09 -0400

    Code update with 'Testing Request Validation & Validating Title & Price'

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -27,12 +27,42 @@ it('returns a status other than 401 if the user is signed in', async () => {
 });
 
 it('returns an error if an invalid title is provided', async () => {
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price: 10
+        })
+        .expect(400);
     
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            price: 10,
+        })
+        .expect(400);    
 });
 
 
 it('returns an error if an invalid price is provided', async () => {
-    
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'Music',
+            price: -10,
+    })
+        .expect(400);   
+
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'Books'
+        })
+        .expect(400);  
 });
 
 
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,21 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@ahtickcon/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@ahtickcon/common';
 
 
 const  router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
+router.post('/api/tickets', requireAuth, [
+    body('title')
+        .not()
+        .isEmpty()
+        .withMessage('Title is required'),
+    body('price')
+        .isFloat({ gt: 0 })
+        .withMessage('Price is must be greater than 0'),
+
+    
+], validateRequest, (req: Request, res: Response) => {
     res.sendStatus(200);
 });
 
