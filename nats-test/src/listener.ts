c9597cf339514dfba4efc4c49590f955c6f49fb2
AliHaidry <alihaidry11@gmail.com>
2021-04-16 17:13:09 -0400

    Code update with directory -- nats-test 'Listening for Data'

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,18 @@
+import nats from 'node-nats-streaming';
+
+console.clear();
+
+const stan = nats.connect('ticketing', '123', {
+    url: 'https://localhost:4222'
+});
+
+stan.on('connect', () =>{
+    console.log('Listener connected to NATS');
+
+    const subscription = stan.subscribe('ticket:created');
+
+    subscription.on('message', (msg) => {
+        console.log('Message received!');
+    });
+
+});
