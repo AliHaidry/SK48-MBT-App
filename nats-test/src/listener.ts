e30e457ffe5dc6d5b0a6a12d3c458b03585a950c
AliHaidry <alihaidry11@gmail.com>
2021-04-16 17:21:09 -0400

    Code update with directory -- nats-test 'Accessing Event Data'

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,5 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
+
 
 console.clear();
 
@@ -11,8 +12,12 @@ stan.on('connect', () =>{
 
     const subscription = stan.subscribe('ticket:created');
 
-    subscription.on('message', (msg) => {
-        console.log('Message received!');
+    subscription.on('message', (msg: Message) => {
+        const data = msg.getData();
+
+        if(typeof data === 'string'){
+            console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+        }
     });
 
 });
