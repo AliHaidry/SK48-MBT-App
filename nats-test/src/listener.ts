73b197b137c912f94ac6430170bb86217f867abd
AliHaidry <alihaidry11@gmail.com>
2021-04-17 11:31:43 -0400

    Code update with directory -- nats-test 'Manual Ack Mode'

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,7 +10,11 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () =>{
     console.log('Listener connected to NATS');
 
-    const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group');
+    const options = stan
+        .subscriptionOptions()
+        .setManualAckMode(true);
+
+    const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group', options);
 
     subscription.on('message', (msg: Message) => {
         const data = msg.getData();
@@ -18,6 +22,9 @@ stan.on('connect', () =>{
         if(typeof data === 'string'){
             console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
         }
+
+        msg.ack();
     });
+
 
 });
