63d979cb849ef4a3fc9c95ad4a06a618a01c4021
AliHaidry <alihaidry11@gmail.com>
2021-04-19 16:09:04 -0400

    Code update with 'More on Publishing'

--- tickets/src/events/publishers/ticket-created-publisher.ts
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -2,5 +2,5 @@ import { Publisher, Subjects, TicketCreatedEvent } from '@ahtickcon/common';
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
     subject: Subjects.TicketCreated = Subjects.TicketCreated;
-    
+
 }
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@ahtickcon/common';
 import  { Ticket } from '../models/ticket';
-
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 
 const  router = express.Router();
 
@@ -25,6 +25,13 @@ router.post('/api/tickets', requireAuth, [
         userId: req.currentUser!.id
     });
     await ticket.save();
+
+    new TicketCreatedPublisher(client).publish({
+        id: ticket.id,
+        title: ticket.title,
+        price: ticket.price,
+        userId: ticket.userId
+    });
 
 
     res.status(201).send(ticket);
