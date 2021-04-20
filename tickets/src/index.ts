48ad625299da31dfa823c133f0b517e73aadecdd
AliHaidry <alihaidry11@gmail.com>
2021-04-20 12:54:11 -0400

    Code update with 'Graceful Shutdown'

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
-/** setting up db */
+/** setting up db! */
 const start = async () => {
   if(!process.env.JWT_KEY){
     throw new Error("JWT_KEY must be defined");
