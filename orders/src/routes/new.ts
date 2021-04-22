ae8aafebe3ab4d8ece3a44c63dd30390c61e867d
AliHaidry <alihaidry11@gmail.com>
2021-04-22 12:40:51 -0400

    Code update with 'Order Creation Logic'

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@ahtickcon/common';
+import { requireAuth, validateRequest, NotFoundError } from '@ahtickcon/common';
 import { body } from 'express-validator';
-
+import { Ticket } from '../models/ticket';
+import { Order} from '../models/order';
 
 const router = express.Router();
 
@@ -13,6 +14,23 @@ router.post('/api/orders', requireAuth, [
         .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
         .withMessage('TicketId must be provided')
 ], validateRequest, async (req: Request, res: Response) => {
+
+    const { ticketId} = req.body;
+    
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if(!ticket){
+        throw new NotFoundError();
+    }
+    // Make sure that this ticket is not already reserved
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created 
+
+
     res.send({});
 }); 
 
