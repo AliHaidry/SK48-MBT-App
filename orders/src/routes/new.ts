36fd37b38c5dc7dc4f21261c0a06e4bf753df4a7
AliHaidry <alihaidry11@gmail.com>
2021-04-22 13:17:25 -0400

    Code update with 'Order Expiration Times'

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -7,6 +7,8 @@ import { Order} from '../models/order';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post('/api/orders', requireAuth, [
     body('ticketId')
         .not()
@@ -23,8 +25,6 @@ router.post('/api/orders', requireAuth, [
         throw new NotFoundError();
     }
 
-  
-
     // Make sure that this ticket is not already reserved
     
     const isReserved = await ticket.isReserved();
@@ -32,15 +32,23 @@ router.post('/api/orders', requireAuth, [
         throw new BadRequestError('Ticket is already reserved');
     }
 
-
     // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
     // Build the order and save it to the database
+    const order = Order.build({
+        userId: req.currentUser!.id,
+        status: OrderStatus.Created,
+        expiresAt: expiration,
+        ticket: ticket
+    });
+    await order.save();
 
     // Publish an event saying that an order was created 
 
 
-    res.send({});
+    res.status(201).send(order);
 }); 
 
 export { router as newOrderRouter };
