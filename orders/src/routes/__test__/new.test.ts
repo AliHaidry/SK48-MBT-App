53558d2e5b4198117968e4b6284d4c11387b503f
AliHaidry <alihaidry11@gmail.com>
2021-04-22 16:54:34 -0400

    Code update with 'Testing the Success Case'

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -39,5 +39,15 @@ it('returns an error if the ticket is already reserved', async () =>{
 
 
 it('reserves a ticket', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20,
+    });
+    await ticket.save();
 
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: ticket.id })
+        expect(201);
 });
