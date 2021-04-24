8c76393dacfe4e3c179339c7b66611e545040a06
AliHaidry <alihaidry11@gmail.com>
2021-04-24 10:37:11 -0400

    Code update with 'Publishing Order Cancellation'

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express';
 import { requireAuth, NotFoundError, NotAuthorizedError } from '@ahtickcon/common';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId',requireAuth, async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if(!order){
         throw new NotFoundError();
@@ -21,6 +23,13 @@ router.delete('/api/orders/:orderId',requireAuth, async (req: Request, res: Resp
     await order.save();
 
     // Publishing an event saying this was cancelled!
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        ticket: {
+            id: order.ticket.id
+        }
+    });
+
 
     res.status(204).send(order);
 }); 
