6479f1adbb87ba208d252c9c285ef0f7f3dc2228
AliHaidry <alihaidry11@gmail.com>
2021-04-24 11:13:35 -0400

    Code update with 'A Few More Reminders'

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,13 +1,14 @@
 import { Message } from 'node-nats-streaming';
 import { Subjects, Listener, TicketCreatedEvent } from '@ahtickcon/common';
 import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
 
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     subject: Subjects.TicketCreated = Subjects.TicketCreated;
-    queueGroupName = 'orders-service';
+    queueGroupName = queueGroupName;
 
     onMessage(data: TicketCreatedEvent['data'], msg: Message){
-        
+
     }
 }
