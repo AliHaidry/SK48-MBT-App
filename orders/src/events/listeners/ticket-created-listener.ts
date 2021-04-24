1ec0f5171a4133b8038099459eefba21d254d9a8
AliHaidry <alihaidry11@gmail.com>
2021-04-24 11:05:58 -0400

    Code update with 'Blueprint for Listeners'

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,13 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, TicketCreatedEvent } from '@ahtickcon/common';
+import { Ticket } from '../../models/ticket';
+
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
+    queueGroupName = 'orders-service';
+
+    onMessage(data: TicketCreatedEvent['data'], msg: Message){
+        
+    }
+}
