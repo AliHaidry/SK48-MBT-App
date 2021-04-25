64b1daf3ff4ea4ffc394ab998ea12e43b5ceb183
AliHaidry <alihaidry11@gmail.com>
2021-04-25 19:23:57 -0400

    Code update with 'One More Test'

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -31,3 +31,18 @@ it("Implements optimistic concurrency control", async (done) => {
 
   throw new Error('Should not be reach this point');
 });
+
+it('Increments the version number on multiple saves', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20,
+        userId: '123'
+    });
+
+    await ticket.save();
+    expect(ticket.version).toEqual(0);
+    await ticket.save();
+    expect(ticket.version).toEqual(1);
+    await ticket.save();
+    expect(ticket.version).toEqual(2);
+});
