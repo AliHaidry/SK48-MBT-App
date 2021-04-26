a1f9028c9783ebf11fb3dc02290edc38e370a304
AliHaidry <alihaidry11@gmail.com>
2021-04-26 13:42:31 -0400

    code update with 'Applying a Version Query'

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,22 +1,29 @@
-import express, { Request, Response } from 'express';
-import { requireAuth, NotFoundError, NotAuthorizedError } from '@ahtickcon/common';
-import { Order, OrderStatus } from '../models/order';
-import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
-import { natsWrapper } from '../nats-wrapper';
+import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from "@ahtickcon/common";
+import { Order, OrderStatus } from "../models/order";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId',requireAuth, async (req: Request, res: Response) => {
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId).populate('ticket');
+    const order = await Order.findById(orderId).populate("ticket");
 
-    if(!order){
-        throw new NotFoundError();
+    if (!order) {
+      throw new NotFoundError();
     }
 
-    if(order.userId !== req.currentUser!.id){
-        throw new NotAuthorizedError();
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
     }
 
     order.status = OrderStatus.Cancelled;
@@ -24,14 +31,14 @@ router.delete('/api/orders/:orderId',requireAuth, async (req: Request, res: Resp
 
     // Publishing an event saying this was cancelled!
     new OrderCancelledPublisher(natsWrapper.client).publish({
-        id: order.id,
-        ticket: {
-            id: order.ticket.id
-        }
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
     });
 
-
     res.status(204).send(order);
-}); 
+  }
+);
 
 export { router as deleteOrderRouter };
