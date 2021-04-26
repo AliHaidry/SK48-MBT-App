ec508979f7b725304494c058bcb22c3760d4f4fc
AliHaidry <alihaidry11@gmail.com>
2021-04-26 18:30:35 -0400

    code update with 'Success Case Testing'

--- orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -38,10 +38,22 @@ const setup = async () => {
 };
 
 it('finds, updates, and saves a ticket', async () => {
+    const { msg, data, ticket, listener } = await setup();
 
+   await listener.onMessage(data,msg);
+
+    const updatedTicket = await Ticket.findById(ticket.id);
+
+    expect(updatedTicket!.title).toEqual(data.title);
+    expect(updatedTicket!.price).toEqual(data.price);
+    expect(updatedTicket!.version).toEqual(data.version);
 });
 
 
 it('acks the message', async () => {
+    const { msg, data, listener} = await setup();
 
+    await listener.onMessage(data, msg);
+
+    expect(msg.ack).toHaveBeenCalled();
 });
