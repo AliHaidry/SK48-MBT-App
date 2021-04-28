d818fe1ca769ba6bad48846a02c4bbdb575ed128
AliHaidry <alihaidry11@gmail.com>
2021-04-28 17:55:13 -0400

    code update with ' ListenerCreation -- expiration directory'

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,12 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@ahtickcon/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+
+    }
+}
