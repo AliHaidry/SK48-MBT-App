e2c862a76246256e9beb3d961de91221d71c538f
AliHaidry <alihaidry11@gmail.com>
2021-04-29 13:39:19 -0400

    code update with 'Queueing a Job on Event Arrival -- expiration directory'

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,17 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@ahtickcon/common';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queueGroupName = queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        await expirationQueue.add({
+            orderId: data.id
+        });
 
+        msg.ack();
     }
 }
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 /** setting up db! */
 const start = async () => {
@@ -32,6 +33,8 @@ const start = async () => {
 
   process.on('SIGINT', () => natsWrapper.client.close());
   process.on('SIGTERM', () => natsWrapper.client.close());
+
+  new OrderCreatedListener(natsWrapper.client).listen();
 
   } catch (err) {
     console.error(err);
