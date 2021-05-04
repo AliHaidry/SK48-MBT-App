6bf0dc744ed115afc64d49be5c1076a6a2208732
AliHaidry <alihaidry11@gmail.com>
2021-05-04 18:13:14 -0400

    code update with 'Mocked Stripe Client -- payments directory'

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,8 @@ import { OrderStatus } from '@ahtickcon/common';
 import { app } from "../../app";
 import { Order } from '../../models/order';
 
+jest.mock('../../stripe');
+
 it("returns a 404 when purchasing an order that does not exist", async () => {
   await request(app)
     .post("/api/payments")
@@ -56,4 +58,25 @@ it("returns a 400 when purchasing a cancelled order", async () => {
         })
         .expect(400);
 
+});
+
+
+it('returns a 204 with valid input', async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+    const order = Order.build({
+        id: mongoose.Types.ObjectId().toHexString(),
+        userId,
+        version: 0,
+        price: 20,
+        status: OrderStatus.Created,
+    });
+    await order.save(); 
+
+    await request(app)
+      .post("/api/payments")
+      .set("Cookie", global.signin(userId))
+      .send({
+        token: 'tok_visa',
+        orderId: order.id
+      });
 });
