0e3237250bdec0b512b8b431cee6f784b285ed80
AliHaidry <alihaidry11@gmail.com>
2021-05-08 19:01:50 -0400

    Code upate with 'Headers Links -- Client Directory'

--- client/components/header.js
+++ client/components/header.js
@@ -4,6 +4,8 @@ export default ({ currentUser }) => {
     const links = [
         !currentUser && {label: 'Sign Up', href: '/authy/signup'},
         !currentUser && {label: 'Sign In', href: '/authy/signin'},
+        currentUser && { label: 'Sell Tickets', href: '/tickets/new'},
+        currentUser && { label: 'My Orders', href: '/orders'},
         currentUser && {label: 'Sign Out', href: '/authy/signout'}
     ]
         .filter(linkConfig => linkConfig)
