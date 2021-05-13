39d0354dbfab9e53b55a474313a1d8fce394c758
AliHaidry <alihaidry11@gmail.com>
2021-05-13 09:43:46 -0400

    Code upated

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -4,7 +4,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 /** setting up db! */
 const start = async () => {
 
-  console.log("Starting the service...");
+  console.log("Starting the service.....");
   if(!process.env.NATS_CLIENT_ID){
     throw new Error("NATS_CLIENT_ID must be defined"); 
   }
